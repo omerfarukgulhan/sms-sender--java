a9605f97be41c7cfb04a7f528ae9475f54db4038
omerfarukgulhan <omerfarukgulhan@outlook.com>
2024-11-11 17:38:11 +0300

    main configurations

--- src/main/java/com/ofg/sms/SmsApplication.java
+++ src/main/java/com/ofg/sms/SmsApplication.java
@@ -2,8 +2,10 @@ package com.ofg.sms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class SmsApplication {
     public static void main(String[] args) {
         SpringApplication.run(SmsApplication.class, args);
